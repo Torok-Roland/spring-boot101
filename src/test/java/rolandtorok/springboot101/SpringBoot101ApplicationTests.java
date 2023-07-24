3f0df5542b5b76a4aa472592581f535e328ac9d5
Torok Roland <torok.roland1998@gmail.com>
2023-07-24 20:19:08 +0300

    Starting springBoot101

---
+++ src/test/java/rolandtorok/springboot101/SpringBoot101ApplicationTests.java
@@ -0,0 +1,13 @@
+package rolandtorok.springboot101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
